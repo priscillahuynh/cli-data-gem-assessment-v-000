a6f0ddcd5dbb55e7629edfba634e91c4b2b5f4bc
Priscilla H <priscillamhuynh@gmail.com>
2018-07-24 08:45:23 +0000

    added initialize, call, and main menu methods

---
+++ meetup/lib/meetup.rb
@@ -0,0 +1,15 @@
+### REQUIRED GEMS
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+### NAMESPACE MODULE ###
+module Meetup
+  # Your code goes here...
+end
+
+### REQUIRED FILES ###
+require_relative "./meetup/version"
+require_relative "./meetup/event"
+require_relative "./meetup/scraper"
+require_relative "./meetup/cli"
